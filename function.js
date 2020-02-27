454070a1cf33d893fdd3af20748b7b811c843571
Henrique Pereira <49798233+HenriqueVPS@users.noreply.github.com>
2020-02-27 17:15:53 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.fa-facebook').click(function(){
+        window.location.replace("https://www.facebook.com/toldoscentralibate/");
+    })
+    $('.fa-instagram').click(function(){
+        window.location.replace("https://www.instagram.com/toldoscentralibate/");    
+    })
+   /* $('.fa-envelope-o').click(function(){
+        window.location.replace("https://outlook.live.com/mail/0/inbox");
+    })*/
+
+    $('.fa-bars').click(function(){
+        $('.mobile').slideToggle();
+    })
+})
